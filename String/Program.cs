f1d333879e101c556e3f783616093ffb3ac06182
Pornpan Seehartrai Madsen <bier0003@edu.sde.dk>
2023-09-01 09:32:14 +0200

    Add project files.

---
+++ String/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddSeparate());
+            Console.WriteLine();
+        }
+
+
+
+
+
+
+        static string AddSparator(string names)
+        {
+            foreach( string name in names) {
+                string newNAme = name.Replace("", "^");
+                return newNAme;
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
